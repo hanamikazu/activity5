cfaf06cd1af6768321cd7768593cb38800b45505
kylam <minsehun9499@gmail.com>
2023-01-12 02:37:11 +0800

    Changing some componenent for design

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,7 +12,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int selectedIndex = 0;
   File? image;
 
 
@@ -30,16 +29,11 @@ class _HomeScreenState extends State<HomeScreen> {
   Future<void> _showOptionsDialog(BuildContext context){
     return showDialog(context: context, builder: (BuildContext context){
       return AlertDialog(
-<<<<<<< HEAD
         title: const Text('Choose From: ', style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),),
-=======
-        title: const Text('Choose From: '),
->>>>>>> 93e539a1f2d338a90a7b8f5de13578f0807a99fa
         content: SingleChildScrollView(
           child: ListBody(
             children: [
               GestureDetector(
-<<<<<<< HEAD
                   child: const Text("Gallery", style: TextStyle(fontSize: 18),),
                   onTap: () async {
                     if (await Permission.storage.request().isGranted){
@@ -69,54 +63,28 @@ class _HomeScreenState extends State<HomeScreen> {
               }, child: const Text("Okay",
               style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),))
           ]
-=======
-                child: const Text("Gallery", style: TextStyle(fontSize: 18),),
-                onTap: (){
-                  chooseFrom(ImageSource.gallery);
-                }
-              ),
-              const Padding(padding: EdgeInsets.all(10)),
-              GestureDetector(
-                child: const Text("Camera", style: TextStyle(fontSize: 18)),
-                onTap: (){
-                  chooseFrom(ImageSource.camera);
-                }
-              )
-            ],
-          ),
-        ),
->>>>>>> 93e539a1f2d338a90a7b8f5de13578f0807a99fa
       );
     });
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.teal,
-        title: const Text("Activity 5 - Martinito"),
-        leading: const Icon(Icons.person),
-      ),
-<<<<<<< HEAD
-      body: DecoratedBox(decoration: const BoxDecoration(
-        image: DecorationImage(
-        image: AssetImage("assets/xiao.jpg"), fit: BoxFit.cover)
-          ),child: Column(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: const Text("Activity 5 - Martinito"),
+          leading: const Icon(Icons.person),
+        ),
+        body: DecoratedBox(decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/xiao.jpg"), fit: BoxFit.cover)
+        ), child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-             children: [
-              Container(
+          children: [
+            Container(
                 color: Colors.teal,
                 margin: const EdgeInsets.all(20),
-                padding: const EdgeInsets.all(30), alignment: Alignment.center,
-=======
-      body: Column(
-        children: [
-          Container(
-            color: Colors.teal,
-            margin: const EdgeInsets.all(20),
-            padding: const EdgeInsets.all(30),
-              alignment: Alignment.center,
->>>>>>> 93e539a1f2d338a90a7b8f5de13578f0807a99fa
+                padding: const EdgeInsets.all(30),
+                alignment: Alignment.center,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
@@ -126,92 +94,86 @@ class _HomeScreenState extends State<HomeScreen> {
                           height: 200,
                           fit: BoxFit.cover,))
                         : const FlutterLogo(size: 200),
-<<<<<<< HEAD
                     const SizedBox(height: 30,),
-                    ElevatedButton(onPressed: (){
+                    ElevatedButton(onPressed: () {
                       _showOptionsDialog(context);
-                      },style: ElevatedButton.styleFrom(backgroundColor: Colors.black54, padding: EdgeInsets.all(12)),
-                        child: const Text("Choose Image", style: TextStyle(fontSize: 20)))
+                    },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black54,
+                            padding: const EdgeInsets.all(12)),
+                        child: const Text(
+                            "Choose Image", style: TextStyle(fontSize: 20)))
                   ],
                 )
             ),
-              Container(
+            Container(
                 padding: const EdgeInsets.only(left: 20, top: 5, right: 20),
                 child: Column(
                   children: const [
                     Text("KYLA MARTINITO",
-                      style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700, color: Colors.white),),
+                      style: TextStyle(fontSize: 32,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white),),
                     SizedBox(height: 10,),
                     Text("BS in Information Technology",
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400, color: Colors.white),),
+                      style: TextStyle(fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white),),
                   ],
                 )
-          ),
-              Container(
-                color: Colors.teal.withOpacity(0.7),
-                padding: const EdgeInsets.all(30),
-                margin: const EdgeInsets.all(20),
-                alignment: Alignment.center,
-                child: Row(
-                  children: const [
-                    SizedBox(
-                      height: 8,
-                    ),
+            ),
+            Container(
+              color: Colors.white.withOpacity(0.7),
+              padding: const EdgeInsets.all(30),
+              margin: const EdgeInsets.all(20),
+              alignment: Alignment.center,
+              child: Row(
+                children: const [
+                  SizedBox(
+                    height: 8,
+                  ),
                   Image(image: AssetImage('assets/instagram_PNG10.png'),
                     height: 50,
                     width: 50,),
-                    Text("  aku_miyu ",
-                      style: TextStyle(color: Colors.white,
-                      fontSize: 18, fontWeight: FontWeight.w600),),
-                   SizedBox(
-                  height: 8,
+                  Text("  aku_miyu ",
+                    style: TextStyle(color: Colors.black,
+                        fontSize: 18, fontWeight: FontWeight.w600),),
+                  SizedBox(
+                    height: 8,
                   ),
                   Image(image: AssetImage('assets/github.png'),
                     height: 70,
                     width: 70,),
-                    Text("hanamikazu",
-                style: TextStyle(color: Colors.white,
-                    fontSize: 18, fontWeight: FontWeight.w600),)
+                  Text("hanamikazu",
+                    style: TextStyle(color: Colors.black,
+                        fontSize: 18, fontWeight: FontWeight.w600),)
                 ],
               ),
             ),
-              ElevatedButton(onPressed: (){
-                showDialog(context: context, builder: (ctx) => AlertDialog(
-                  title: const Text("!! Final Output !!",
-                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 24),),
-                  content: const Text("Activity 5 - Mobile Programming",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),),
-                  actions: <Widget>[
-                    TextButton(onPressed: () {
-                      Navigator.of(ctx).pop();
+            ElevatedButton(onPressed: () {
+              showDialog(context: context, builder: (ctx) =>
+                  AlertDialog(
+                    title: const Text("!! Final Output !!",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700, fontSize: 24),),
+                    content: const Text("Activity 5 - Mobile Programming",
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.w500),),
+                    actions: <Widget>[
+                      TextButton(onPressed: () {
+                        Navigator.of(ctx).pop();
                       }, child: const Text("Okay",
-                      style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),))
-                  ],
-                ));
-              },
-               style: ElevatedButton.styleFrom(backgroundColor: Colors.black54, padding: const EdgeInsets.all(12)),
-                child: const Text("CLICK ME !!", style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500)))
-        ],
-      ),
-      ));
-=======
-                    ElevatedButton(onPressed: (){
-                      _showOptionsDialog(context);
-                      },style: ElevatedButton.styleFrom(backgroundColor: Colors.black54),
-                        child: const Text("Choose Image", style: TextStyle(fontSize: 20),))
-                  ],
-                )
-            ),
-          Container(
-            padding: const EdgeInsets.only(left: 20, top: 5, right: 20),
-            child: const Text("ALATUS",
-              style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),),
-          ),
-          const Text("Conqueror of Demons",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),),
-        ],
-      ),
-    );
->>>>>>> 93e539a1f2d338a90a7b8f5de13578f0807a99fa
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 16),))
+                    ],
+                  ));
+            },
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.black54,
+                    padding: const EdgeInsets.all(12)),
+                child: const Text("CLICK ME !!", style: TextStyle(
+                    fontSize: 30, fontWeight: FontWeight.w500)))
+          ],
+        ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,6 @@ void main() {
       colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.black54),
     ),
     home: const HomeScreen()
-<<<<<<< HEAD
     )
-
   );
-=======
-
-  ));
->>>>>>> 93e539a1f2d338a90a7b8f5de13578f0807a99fa
 }
